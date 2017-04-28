017ae741969f1a513d6a6e476f6c53990cc94730
CFINPC\Finst <finsternavy@gmail.com>
2017-04-27 21:11:17 -0700

    Time Conversion 1 assignment.

---
+++ Topic 3/TimeConversion_1.java
@@ -0,0 +1,30 @@
+package demo;
+
+import java.util.Scanner;
+
+public class TimeConversion_1 {
+
+	public static void main(String[] args) {
+
+		Scanner input = new Scanner(System.in);
+		
+		System.out.print("Enter an hour in the 24 hour format: ");
+		int hour = input.nextInt();
+		
+		if (hour < 0 || hour > 24) {
+			System.out.println("Invalid entry");
+			System.exit(0);
+		} else if (hour == 0) {
+			System.out.print("12am.");
+		} else if (hour == 12) {
+			System.out.print("12pm.");
+		} else if (hour >= 12) {
+			System.out.print(hour % 12 + "pm.");
+		} else {
+			System.out.print(hour % 12 + "am.");
+		}
+		
+
+	}
+
+}
