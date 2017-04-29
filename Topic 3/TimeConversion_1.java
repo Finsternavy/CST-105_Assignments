281ce82d740188b632a2f3c8329697d83bac6743
CFINPC\Finst <finsternavy@gmail.com>
2017-04-28 17:54:40 -0700

    TimeConversion_1 update

--- Topic 3/TimeConversion_1.java
+++ Topic 3/TimeConversion_1.java
@@ -1,29 +1,38 @@
 package demo;
 
 import java.util.Scanner;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Locale;
 
 public class TimeConversion_1 {
 
+	
 	public static void main(String[] args) {
-
+		
+		
 		Scanner input = new Scanner(System.in);
 		
-		System.out.print("Enter an hour in the 24 hour format: ");
-		int hour = input.nextInt();
 		
-		if (hour < 0 || hour > 24) {
-			System.out.println("Invalid entry");
-			System.exit(0);
-		} else if (hour == 0) {
-			System.out.print("12am.");
-		} else if (hour == 12) {
-			System.out.print("12pm.");
-		} else if (hour > 12) {
-			System.out.print(hour % 12 + "pm.");
-		} else {
-			System.out.print(hour % 12 + "am.");
-		}
+		System.out.print("Enter a time in 24-hour format: ");
 		
+
+	    String time = input.nextLine();
+		String time24 = time;
+
+	    Integer timeint = Integer.parseInt(time24);
+
+	    Calendar calendar = Calendar.getInstance();
+
+	    calendar.set(Calendar.HOUR_OF_DAY, timeint);
+
+	    calendar.set(Calendar.MINUTE, 0);
+
+	    SimpleDateFormat sdf = new SimpleDateFormat("ha");
+
+	    String time12 = sdf.format(calendar.getTime());
+
+	    System.out.println(time12);
 
 	}
 
